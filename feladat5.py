97896c99f4ec5a4669d2ecec4979ee0fa18397df
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2023-03-07 10:07:41 +0100

    5f

--- feladat5.py
+++ feladat5.py
@@ -1,1 +1,9 @@
-#5f
+from math import sqrt
+
+szam=int(input("Írj be egy számot: "))
+
+if szam<=0:
+    print("pozitív számmal dolgozz!")
+    int(input("Írj be egy új számot: "))
+else:
+    print("A megadott szám négyzetgyöke: ",round(sqrt(szam),2))
