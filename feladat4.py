64ea94dbbdac4a44690f2870eeb126fd3908b8f4
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2023-03-07 10:00:19 +0100

    4f

--- feladat4.py
+++ feladat4.py
@@ -1,5 +1,6 @@
 import math
 
-atmero=input("Írd be a kör átmérőjét: ")
-print("Kerülete: ", atmero*math.pi)
-print("Területe: ",(atmero/2)**2*math.pi)
+atmero=int(input("Írd be a kör átmérőjét(cm): "))
+pi=math.pi
+print("Kerülete: ",round(atmero*pi,2),"cm")
+print("Területe: ",round((atmero/2)**2*pi,2),"cm")
