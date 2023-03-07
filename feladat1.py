c41b6993a116c688505f999c354c23a6d12ea051
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2023-03-07 10:44:01 +0100

    f edit

--- feladat1.py
+++ feladat1.py
@@ -1,5 +1,5 @@
 n=int(input("Hány évig szeretne kamatoztatni: "))
 x=int(input("Mekkora összeget szeretne kamatoztatni(Ft): "))
-lekötés=int(input("Lekötés kamata: "))
+lekotes=int(input("Lekötés kamata: "))
 
-print(n, "év után",round(x*(1+5/100)**n),"Ft-t kaphatunk a betéteink után.")
+print(n,"év után",round(x*(1+5/100)**n),"Ft-t kaphatunk a betéteink után.")
